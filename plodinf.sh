41715d95fe6f8d530e7508e57f3ab6f2b1e485ba
juanjo255 <piconcossio@gmail.com>
2024-06-17 17:36:10 -0500

    sorting in functions and adding flags

--- plodinf.sh
+++ plodinf.sh
@@ -10,9 +10,9 @@ plodinf_help() {
     PLODINF - PLOidy INFerece for RAD-seq
 
     Authors:
-	Andres Florian Cruz
+	    Andres Florian Cruz
     	Juan Picon Cossio
-	Elisa Correa Perez
+	    Elisa Correa Perez
 
     Version: 0.1
 
@@ -44,10 +44,32 @@ while getopts 'i:t:m:w:r:d' opt; do
         prefix=$OPTARG
         ;;
         d)
-        output_dir="mitnanex_results_$(date  "+%Y-%m-%d_%H-%M-%S")/"
+        output_dir="plodinf_results_$(date  "+%Y-%m-%d_%H-%M-%S")/"
         ;;
         *)
         plodinf_help
         ;;
     esac 
 done
+
+
+
+get_snp_count (){
+    ## Get the read count for each SNP
+    bcftools mpileup -Q 20 -q 30 --skip-all-unset 3 -a "FORMAT/AD" -Ou --no-reference $bam_file 
+}
+
+filter_snp(){
+    ## Filter SNPs by coverage and quality
+    bcftools filter -Ou -e 'INFO/QS[1] == 1 | INFO/DP <= 10' -
+}
+    
+format_snp_file (){
+    ## Change the format for the outputted file
+    bcftools query -f '%CHROM\t%POS\t%ALT\t%DP\tQS:%QS[\t%AD]]\n' 
+}
+
+compute_dist_snp(){
+    ## Compute the distribution of reads to assign a ploidy base on that distribution
+    python src/allele_counts_parser.py -m $minimum_ad -d $minimum_depth > $out_path/$prefix\_$pop\_allele_prop_m$minimum_ad\_d$minimum_depth.tsv
+}
